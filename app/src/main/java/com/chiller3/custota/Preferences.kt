add62b071668b1d9a280f3a9718f92feac1ac1ef
Benjamin Daines <benjamindaines@gmail.com>
2026-06-13 16:20:28 -0400

    Set up for testing final deployment

--- app/src/main/java/com/chiller3/custota/Preferences.kt
+++ app/src/main/java/com/chiller3/custota/Preferences.kt
@@ -24,7 +24,7 @@ class Preferences(initialContext: Context) {
     companion object {
         private val TAG = Preferences::class.java.simpleName
 
-        const val DEFAULT_OTA_SOURCE = "https://ota.dgsd.ph/testing"
+        const val DEFAULT_OTA_SOURCE = "https://ota.dgsd.ph/robertjohnson-test"
 
         // Keep in the same order as the helper functions below.
         private const val PREF_ALREADY_MIGRATED = "already_migrated"
